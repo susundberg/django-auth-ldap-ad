df86add8b7717eddb0431463f895572976032b74
Pauli Sundberg <pauli.sundberg@ssf.fi>
2014-08-29 16:30:04 +0300

    Import sources

---
+++ djago-auth-ldap-ad/backend.py
@@ -0,0 +1,156 @@
+
+import ldap,ldap.sasl
+from django.contrib.auth.models import User, Group
+
+
+class LDAPBackendException( Exception ):
+   pass
+
+"""
+ Main class for handling the authentication
+"""
+class LDAPBackend(object):
+   
+    def authenticate(self, username=None, password=None):
+        if hasattr( self, "ldap_settings") == False:
+           self.ldap_settings = LDAPSettings()
+        
+        # For all configured servers try to connect
+        for server in self.ldap_settings.SERVER_URI.split(","):
+           
+           # Use self.ldap_connection object if such is given for testing with mockldap.
+           if hasattr( self, "ldap_connection" ) == False:
+              try:
+                 ldap_connection = self.ldap_open_connection(server)
+              except ldap.SERVER_DOWN:
+                  continue
+              except ldap.INVALID_CREDENTIALS:
+                  return None
+           else:
+              ldap_connection = self.ldap_connection
+              
+           for key,value in self.ldap_settings.CONNECTION_OPTIONS.items():
+              ldap_connection.set_option( key , value )
+
+           # Do search
+           try:
+              ldap_user_info = self.ldap_search_user( ldap_connection,username,password ) 
+           except LDAPBackendException:
+              return None
+           
+           return self.get_local_user( username, ldap_user_info )
+        return None
+        
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+         
+
+    def ldap_open_connection( self, ldap_url, username, password ):
+         ldap_session = ldap.initialize(ldap_url,trace_level=self.ldap_settings.TRACE_LEVEL)
+         
+         sasl_auth = ldap.sasl.sasl( {
+               ldap.sasl.CB_AUTHNAME:username,
+               ldap.sasl.CB_PASS    :password,
+               },
+               self.ldap_settings.SASL_MECH
+            )
+         
+         connection.sasl_interactive_bind_s("", sasl_auth)
+         return ldap_session
+    
+    # Search for user, returns users info (dict)
+    def ldap_search_user(self, connection, username, password ):
+
+
+      result_set = []
+      ldap_result_id = connection.search( self.ldap_settings.SEARCH_DN , ldap.SCOPE_SUBTREE, self.ldap_settings.SEARCH_FILTER % { "user" : username }) 
+      result_all_type, result_all_data = connection.result(ldap_result_id, 1)
+      result_entries = []
+      for result_type, result_data in result_all_data:
+         if result_type != None:
+            result_entries.append( result_data )
+            
+      if len( result_entries ) == 0:
+         raise LDAPBackendException("No entries found!")
+      
+      if len( result_entries ) != 1:
+         raise LDAPBackendException("More than one found!")
+
+      return result_entries[0]
+
+    def get_local_user(self, ldap_username, info ):
+       username = ldap_username.lower()
+       try:
+          user = User.objects.get( username = username )
+       except User.DoesNotExist:
+          # Make new one
+          user = User( username = username )
+          user.set_unusable_password()
+       # refresh memberships
+       members_of = []
+       for group in info['memberOf']:
+          members_of.append( group.lower().split(",") )
+          
+       
+
+       # Set first_name or last_name or email ..
+       for key, value in self.ldap_settings.USER_ATTR_MAP.items():
+          if value in info:
+             setattr( user, key, info[value][0] )
+
+       
+
+       def check_for_membership( members_of, required_groups):
+          # check for all members of groups
+          for member_of_group in members_of:
+             # check that all required groups are in this membership
+             requirement_fullfilled = all( required_group in member_of_group for required_group in required_groups )
+             if requirement_fullfilled:
+               return True
+          return False
+
+       # set is_superuser
+       for key, value in self.ldap_settings.USER_FLAGS_BY_GROUP.items():
+          # cn=superuser,ou=paulus
+          required_groups = value.lower().split(",")
+          setattr( user, key, check_for_membership( members_of, required_groups ))
+       
+       # We need to do save before we can use the groups (for m2m binding)
+       user.save()
+       
+       # user.groups.add( Group.objects.get(name = "ODAdmin"))
+       for key, value in self.ldap_settings.USER_GROUPS_BY_GROUP.items():
+          required_groups = value.lower().split(",")
+          if check_for_membership( members_of, required_groups ):
+              user.groups.add( Group.objects.get(name=key) )
+          else:
+              user.groups.remove( Group.objects.get(name=key) )
+       user.save()
+       return user
+       
+
+         
+""" Load settings from Django settigns """
+class LDAPSettings(object):
+    defaults = {
+        'CONNECTION_OPTIONS': { ldap.OPT_REFERRALS : 0},
+        'SERVER_URI': 'ldap://localhost',
+        'USER_FLAGS_BY_GROUP' : {},
+        'USER_GROUPS_BY_GROUP' : {},
+        'USER_ATTR_MAP' : {},
+        'TRACE_LEVEL' : 3,
+        'SASL_MECH' : 'DIGEST-MD5',
+        'SEARCH_DN'     : "DC=localdomain,DC=ORG",
+        'SEARCH_FILTER' : "(SAMAccountName=%(user)s)"
+    }
+
+    def __init__(self, prefix='AUTH_LDAP_'):
+        from django.conf import settings
+
+        for name, default in self.defaults.items():
+            value = getattr(settings, prefix + name, default)
+            setattr(self, name, value)
+
