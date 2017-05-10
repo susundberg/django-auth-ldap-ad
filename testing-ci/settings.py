c6519bbbfea705aad830e050715042cc3e8a904e
Seppo Yli-Olli <seppo.yliolli@gmail.com>
2017-05-10 17:26:58 +0300

    Fix reference to module name

--- testing-ci/settings.py
+++ testing-ci/settings.py
@@ -9,7 +9,7 @@ DATABASES = {
         }
 }
 
-INSTALLED_APPS=["django-auth-ldap-ad","django.contrib.auth", "django.contrib.contenttypes"]
+INSTALLED_APPS=["django_auth_ldap_ad","django.contrib.auth", "django.contrib.contenttypes"]
 DEBUG=True
 
 SECRET_KEY="92f7zw@-pl+pc2seg=*sfc7hkrx3h-8+jgsfzay#p)9$t(6@@t"
